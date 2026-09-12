e18968f0ffc5eb7fef69e6f52a6726c7c70dbb04
rsnwms2tpm-lang <rsnwms2tpm@privaterelay.appleid.com>
2026-09-12 13:53:12 +0100

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE='track-day-log-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const req=event.request;
+  const nav=req.mode==='navigate'||(req.headers.get('accept')||'').includes('text/html');
+  if(nav){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html').then(x=>x||caches.match('./'))));return}
+  event.respondWith(caches.match(req).then(cached=>cached||fetch(req).then(res=>{if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy))}return res}).catch(()=>cached)));
+});
