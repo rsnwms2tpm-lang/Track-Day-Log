aa7ed304bcb87b587a0480930b32adffc388333b
rsnwms2tpm-lang <rsnwms2tpm@privaterelay.appleid.com>
2026-09-12 15:14:17 +0100

    Bump offline cache for staged preparation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='track-day-log-v4';
+const CACHE='track-day-log-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
